d812e80150b2ef18248cfcd6c677f96c8830fbca
alex005489465 <alextree008@outlook.com>
2024-12-11 10:00:25 +0800

    Convenient to use postman testing function, Disable CSRF token when developing

--- backend/my-project/app/Providers/AppServiceProvider.php
+++ backend/my-project/app/Providers/AppServiceProvider.php
@@ -23,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+        
+        // Disable CSRF token for all endpoints when developing
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::except([
+            '/*'
+        ]);
     }
 }
