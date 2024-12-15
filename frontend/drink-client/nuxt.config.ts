b69d0d5fe2d8a2c3bee89d50eeb94ade06417bfc
alex005489465 <alextree008@outlook.com>
2024-12-15 20:34:21 +0800

    cookie test wrong;

--- frontend/drink-client/nuxt.config.ts
+++ frontend/drink-client/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -9,10 +10,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: [
-    '@nuxtjs/color-mode', 
-    '@pinia/nuxt'
-  ],
+  modules: ['@nuxtjs/color-mode', '@pinia/nuxt', '@vueuse/nuxt'],
   /*
   colorMode: {
     preference: 'system', // Default theme: 'light', 'dark' or 'system'
