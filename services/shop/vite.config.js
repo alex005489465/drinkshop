c3f625cf9d7409be00b46daaeeb9dc68544f79cb
alex005489465 <alextree008@outlook.com>
2025-01-22 15:19:14 +0800

    init with breeze&blade

--- services/shop/vite.config.js
+++ services/shop/vite.config.js
@@ -1,36 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
-            //refresh: true,
-            refresh: [
-                'resources/stores/**',
-                'app/Livewire/**',
-                'app/View/Components/**',
-                'lang/**',
-                'resources/lang/**',
-                'resources/views/**',
-                'routes/**'
-            ],
-            assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg', '**/*.gif', '**/*.webp', '**/*.ico', '**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.eot'],
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-            '@images': '/resources/images',
-        },
-    },
 });
