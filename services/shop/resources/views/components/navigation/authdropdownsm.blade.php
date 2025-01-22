465d09972030d6163c70cec45a6dc6885803d31c
alex005489465 <alextree008@outlook.com>
2025-01-22 20:22:30 +0800

    front page adjust ok.

---
+++ services/shop/resources/views/components/navigation/authdropdownsm.blade.php
@@ -0,0 +1,41 @@
+<div class="pt-4 pb-1 border-t border-gray-200">
+    @if (Route::has('login'))
+        @auth
+            <div class="px-4">
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+            </div>
+
+            <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('profile.edit')">
+                    {{ __('Profile') }}
+                </x-responsive-nav-link>
+
+                <!-- Authentication -->
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-responsive-nav-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-responsive-nav-link>
+                </form>
+            </div>
+        @else
+            <div class="px-4">
+                <div class="font-medium text-base text-gray-800">{{ __('Hi Guest') }}</div>
+            </div>
+            <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('login')">
+                    {{ __('Log in') }}
+                </x-responsive-nav-link>
+                @if (Route::has('register'))
+                    <x-responsive-nav-link :href="route('register')">
+                        {{ __('Register') }}
+                    </x-responsive-nav-link>
+                @endif
+            </div>
+        @endauth
+    @endif
+</div>
